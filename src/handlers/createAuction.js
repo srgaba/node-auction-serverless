977253a61c45f1f6973a2f480131ff34a4eefa68
gabriel cerqueira <gabriel.moraes@meta.com.br>
2021-07-21 20:45:13 -0300

    chore/dynamo

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,11 +1,22 @@
+import { v4 as uuid } from "uuid";
+import AWS from "aws-sdk";
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 async function createAuction(event, context) {
   const { title } = JSON.parse(event.body);
 
   const auction = {
+    id: uuid(),
     title,
     status: "OPEN",
     createdAt: new Date().toISOString(),
   };
+
+  dynamodb.put({
+    TableName: "AuctionsTable",
+    Item: auction,
+  });
 
   return {
     statusCode: 200,
